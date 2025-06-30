968ea125d955bb6d47a8358af58c3825a0e527b0
FrotaLucas <lucasdiasfrota@gmail.com>
2025-06-30 16:02:51 +0200

    delete unused lines

--- TestBrowser/Steps/CreateEmployee.cs
+++ TestBrowser/Steps/CreateEmployee.cs
@@ -15,26 +15,26 @@ namespace TestBrowser.Steps
 
             var dialogContainer = BrowserInstance.FindElement(By.XPath("//*//body/div/div/div/mat-dialog-container"));
 
-            //dialog-content
-            //element = dialogContainer.FindElement(By.XPath(".//div/div/app-agregar-editar-persona/form/mat-dialog-content"));
-
             //4 divs
             var felds = dialogContainer.FindElements(By.XPath(".//div/div/app-agregar-editar-persona/form/mat-dialog-content/div"));
 
             //Name
-            //element = felds[0].FindElement(By.XPath(".//div/div/mat-form-field/div/div/div/input"));
             element = felds[0].FindElement(By.XPath(".//div/mat-form-field/div/div/div/input"));
 
             element.Click();
             element.SendKeys("Lucas");
 
             //Email
-            //element = felds[0].FindElement(By.XPath(".//div/div/mat-form-field/div/div/div/input"));
             element = felds[1].FindElement(By.XPath(".//div/mat-form-field/div/div/div/input"));
 
             element.Click();
             element.SendKeys("lucas_frota@hotmail.com");
 
+            //Type Document
+            element = felds[1].FindElement(By.XPath(".//div/mat-form-field/div/div/div/input"));
+
+            element.Click();
+            element.SendKeys("lucas_frota@hotmail.com");
 
             Thread.Sleep(20 * Constants.StandartWaitTime);
 
