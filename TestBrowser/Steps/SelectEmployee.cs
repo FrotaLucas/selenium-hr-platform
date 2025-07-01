5ec6dd9d9ad3559a5c38347a8ff30ef7c78cb9ee
FrotaLucas <lucasdiasfrota@gmail.com>
2025-07-01 16:00:18 +0200

    add wait times

--- TestBrowser/Steps/SelectEmployee.cs
+++ TestBrowser/Steps/SelectEmployee.cs
@@ -17,10 +17,12 @@ namespace TestBrowser.Steps
             var filter = BrowserInstance.FindElement(By.XPath("//*//app-root/app-list-personas/mat-card/div/mat-form-field/div/div/div/input"));
             filter.Click();
             filter.SendKeys(stepValue);
+            Thread.Sleep(Constants.StandartWaitTime);
 
             var garbageRow = BrowserInstance.FindElements(By.XPath("//*//app-root/app-list-personas/mat-card/table/tbody/tr/td")).Last();
             var deleteButton = garbageRow.FindElements(By.TagName("mat-icon")).Last();
             deleteButton.Click();   
+            Thread.Sleep(Constants.StandartWaitTime);
 
             return true;
 
