d5e66a0e52fff835f9ee1b7022c2c8cc566212ba
FrotaLucas <lucasdiasfrota@gmail.com>
2025-07-01 16:20:07 +0200

    use constructor of StepBase

---
+++ TestBrowser/Steps/DeleteEmployee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace TestBrowser.Steps
+{
+    internal class DeleteEmployee : StepBase
+    {
+        public DeleteEmployee() : base("deleteEmployee")
+        {
+            
+        }
+
+        public override bool Step(IWebDriver BrowserInstance, string? stepValue, string? stepPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
